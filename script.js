7d5c88e3c7f833a957f6420f1718f6e3c5c6e1db
Dev Silvio <devsilviomartins@gmail.com>
2024-05-24 13:21:55 -0300

    formulario-js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('formulario').addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const campoA = parseInt(document.getElementById('campoA').value);
+    const campoB = parseInt(document.getElementById('campoB').value);
+    const message = document.getElementById('message');
+
+    if (campoB > campoA){
+        message.textContent = "formulário valido B é maior que A";
+        message.style.display = "block"
+    }else {
+        message.textContent = "formulário invalido A é maior que B"
+        message.style.color = "red"
+        message.style.display = "block"
+    }
+})
